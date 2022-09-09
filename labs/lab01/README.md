6cacee3a73c723fb18c021796d34c559e1f2f198
ufidon <ufidon@gmail.com>
2022-09-09 11:30:02 -0400

    lab01 added

--- labs/lab01/README.md
+++ labs/lab01/README.md
@@ -1,4 +1,57 @@
-# Lab01: Unit Converter
+# Lab01: A Toy Lottery Game
 
-under construction...
+## 1. Description
+This toy lottery game is played in command prompt window. It randomly generates 4 digits, there could be duplicate digits. Then you are asked to guess those 4 digits. It has 9 single letter commands now. These commands are
+
+```batch
+Welcome to the lottery game!
+============================
+The game starts asking for a command:
+        h: show this help information
+        n: start a new game
+        p: print lottery results
+        o: sort the awards in decreasing order
+        d: display the statistics about the awards
+        t: test the program
+        s: save lotteries played
+        l: load lotteries played
+        q: quit the game
+```
+
+## 2. Objectives
+Practice programming paradigms of C++ arrays:
+1. iterate an array
+2. find the maximum/minimum value of a number array
+3. find the sum/average/standard deviation of a number array
+4. sort a number array
+5. write module code for a given program
+6. test user code and given framework code together
+
+## 3. Tasks
+1. (10%) Play with the game, try each command. Take a screenshot of the output of each command your played.
+   1. Copy test-records.txt and ref-results.txt to your project folder before you run this lottery game in Visual Studio
+   2. Copy test-records.txt and ref-results.txt to the folder containing your lotter.exe before you run this lottery game in command prompt
+2. (80%, 20% for each) Complete the 4 TODOs in lottery.cpp
+3. (10%) Test your implementation using the t command
+   1. Copy test-records.txt and ref-results.txt to the folder containing your lotter.exe before you run this lottery game in command prompt
+   2. run the t command to test your implementation automatically and generate a test-results.txt
+   3. then compare your test-results.txt with the ref-results.txt, you may use command fc:
+   ```batch
+   fc test-results.txt ref-results.txt
+   ```
+   If everything is right, there should be NO difference between these two files. 
+
+## 4. What to submit
+1. Rename your lottery.cpp to be your FirstName_LastName_lottery.cpp
+2. A lab report with screenshots that show 
+## 5. Rubrics
+
+| Credits | Criteria |
+| ------- | -------- |
+| 0% | Source code files were not provided.<br> Problem solution was not submitted. |
+| 25% | Significant assignment requirements were ignored or violated.<br>Program doesn’t compile. |
+| 50% | Output of the program was not shown.<br> Lack of comments.<br> Pour code readability (inconsistent indentation, variable naming, general organization) |
+| 75% | Choosing a poorly approach to solve a problem, for example, solving a problem with hard coding instead of using a loop.<br> Minor details of the program specifications were violated. |
+| 100% | Program works correctly and meets the requirements of the assignment. <br> Code is clean, well-organized, and well commented. |
+
 
---
+++ labs/lab01/code/lottery.cpp
@@ -0,0 +1,411 @@
+// This lab (project) is an extension of the book example "Lottery Using Strings"
+// https://liveexample.pearsoncmg.com/liang/cpp3e/html/LotteryUsingStrings.html
+
+#include <iostream>
+#include <iomanip>
+#include <string> // for using strings
+#include <ctime> // for time function
+#include <cstdlib> // for rand and srand functions
+#include <fstream>
+#include <cmath>
+using namespace std;
+
+#define PLAYERS     10 // number of players
+#define LOTTERYBALLS    4 // number of digits in the lottery
+
+// global data structure
+// 10 elements array, fill in circularly, if it is played 11 times, 
+// the information of the first player will be overwritten
+string gPlayerNames[PLAYERS];
+string gPlayerGuess[PLAYERS];
+string gPlayerLottery[PLAYERS];
+int gPlayerAward[PLAYERS];
+int gSortedAward[PLAYERS];
+int gcurrentplayer = 0; // to index global arrays
+int gtimesplayed = 0; // how many times played so far?
+
+
+// function forward declaration
+string GenerateLottery();
+string GuessLottery();
+int DecideAward(const string& lottery, const string& guess);
+void SortAwards(); // in decreasing order
+void PrintResults();
+void DoAwardStatistics();
+void SaveLottery();
+void LoadLottery();
+void TestLottery();
+
+int main()
+{
+
+	// 1. initialize the ramdom number generator
+	// Refer to: https://liveexample.pearsoncmg.com/liang/cpp3e/html/LotteryUsingStrings.html
+	srand(time(0));
+
+	// help information
+	cout << "Welcome to the lottery game!\n";
+	cout << "============================\n";
+
+	string helpinfo = "The game starts asking for a command:\n\
+        h: show this help information\n\
+        n: start a new game\n\
+        p: print lottery results\n\
+        o: sort the awards in decreasing order\n\
+        d: display the statistics about the awards\n\
+        t: test the program\n\
+        s: save lotteries played\n\
+        l: load lotteries played\n\
+        q: quit the game\n\
+        ";
+	cout << helpinfo << endl;
+
+	// variables
+	char command = 'n';
+	char yn = 'n';
+	string lottery, guess;
+	string playername;
+
+
+	// main lottery logic
+	while (true)
+	{
+		cout << "Please enter your command: ";
+		cin >> command;
+		switch (command)
+		{
+		case 'h':
+		case 'H':
+			cout << helpinfo << endl;
+			break;
+		case 'n':
+		case 'N':
+			cout << "Please enter your name: ";
+			//cin.clear(); // refer to: https://stackoverflow.com/questions/7786994/c-getline-isnt-waiting-for-input-from-console-when-called-multiple-time
+			//cin.sync();
+			//cin.ignore();
+			//getline(cin, gPlayerNames[gcurrentplayer]);
+			cin >> gPlayerNames[gcurrentplayer];
+
+
+			gPlayerLottery[gcurrentplayer] = GenerateLottery();
+			gPlayerGuess[gcurrentplayer] = GuessLottery();
+
+			gPlayerAward[gcurrentplayer] = DecideAward(gPlayerLottery[gcurrentplayer], gPlayerGuess[gcurrentplayer]);
+			// Move to the next slot of the arrays
+			gcurrentplayer++;
+			gcurrentplayer %= PLAYERS; // wrap back to the beginning when the arrays are filled up
+
+			gtimesplayed++;
+			break;
+		case 'l':
+		case 'L':
+			cout << "loading lottery records..." << endl;
+			LoadLottery();
+			break;
+		case 'o':
+		case 'O':
+			SortAwards();
+			break;
+		case 's':
+		case 'S':
+			cout << "saving lottery records..." << endl;
+			SaveLottery();
+			break;
+		case 't':
+		case 'T':
+			TestLottery();
+			break;
+		case 'p':
+		case 'P':
+			PrintResults();
+			break;
+		case 'd':
+		case 'D':
+			DoAwardStatistics();
+			break;
+		case 'q':
+		case 'Q':
+			cout << "Do you really want to quit? (y/n)?" << endl;
+			cin >> yn;
+			if (yn == 'y' || yn == 'Y')
+			{
+				return 1;
+			}
+			break;
+
+		default:
+			cout << "Unknow command!" << endl;
+			break;
+		}
+	}
+
+	return 0;
+}
+
+
+// function implementation
+string GenerateLottery() {
+	// refer to: https://liveexample.pearsoncmg.com/liang/cpp3e/html/LotteryUsingStrings.html
+	string lottery;
+	int digit = 0;
+	for (int i = 0; i < LOTTERYBALLS; i++)
+	{
+		digit = rand() % 10;
+		lottery += static_cast<char>(digit + '0');
+	}
+	return lottery;
+}
+
+string GuessLottery() {
+	string guess;
+	bool alldigits = false;
+	cout << "To guess the lottery," << endl;
+	do
+	{
+		cout << "Please enter " << LOTTERYBALLS << " consecutive digits: " << endl;
+		cin >> guess;
+		if (guess.size() != LOTTERYBALLS)
+		{
+			cout << "Please enter exactly " << LOTTERYBALLS << " digits!" << endl;
+			continue;
+		}
+
+		alldigits = true;
+		for (int i = 0; i < guess.size(); i++)
+		{
+			alldigits = alldigits && isdigit(guess[i]);
+		}
+		if (!alldigits)
+		{
+			cout << "Not all digits in your guess!" << endl;
+		}
+	} while (!alldigits);
+
+	return guess;
+}
+
+int DecideAward(const string& lottery, const string& guess) {
+	// exact match
+	if (guess == lottery)
+	{
+		cout << "Exact match: you win $10,000!" << endl;
+		return 1000;
+	}
+	// match 3 consecutive digits, pay attention to the hard code here
+	else if (guess.substr(0, 3) == lottery.substr(0, 3) ||
+		guess.substr(1, 3) == lottery.substr(1, 3)) // match LOTTERYBALLS-1 digits consecutively
+	{
+		cout << "Match " << LOTTERYBALLS - 1 << " consecutive digits: you win $5000!" << endl;
+		return 500;
+	}
+	// match 2 consecutive digits
+	else if (guess.substr(0, 2) == lottery.substr(0, 2) ||
+		guess.substr(1, 2) == lottery.substr(1, 2) ||
+		guess.substr(2, 2) == lottery.substr(2, 2)
+		)
+	{
+		cout << "Match " << LOTTERYBALLS - 2 << " consecutive digits: you win $2500!" << endl;
+		return 250;
+	}
+	// math 1 digit in place
+	else if (guess[0] == lottery[0] ||
+		guess[1] == lottery[1] ||
+		guess[2] == lottery[2] ||
+		guess[3] == lottery[3]
+		)
+	{
+		cout << "Match " << LOTTERYBALLS - 3 << " digits in place, you win $1000!" << endl;
+		return 100;
+	}
+	else
+	{
+		cout << "Nothing match, $10 is awarded as encouragement." << endl;
+		return 10;
+	}
+}
+
+void SortAwards() {
+	// Refer to: https://liveexample.pearsoncmg.com/liang/cpp3e/html/SelectionSort.html
+	// find the current number of records
+	int records = PLAYERS;
+	if (gtimesplayed < PLAYERS)
+	{
+		records = gtimesplayed;
+	}
+	// copy gPlayerAward to gSortedAward, then sort gSortedAward
+	for (int i = 0; i < records; i++)
+	{
+		gSortedAward[i] = gPlayerAward[i];
+	}
+
+	
+	// TODO 1.================================================
+	// sort gSortedAward[records] with records integers
+
+
+	// END OF TODO 1.=========================================
+
+	//print the sorted awards
+	cout << "The awards in decreasing order:" << endl;
+	for (int i = 0; i < records; i++)
+	{
+		cout << gSortedAward[i] << " ";
+	}
+	cout << endl;
+}
+
+void PrintResults() {
+	int records = PLAYERS;
+	if (gtimesplayed < PLAYERS)
+	{
+		records = gtimesplayed;
+	}
+	cout << "**********" << setw(2) << records << " lottery records **************" << endl;
+	// refer to: checkpoint 4.22-27
+	// 1. https://liveexample.pearsoncmg.com/liang/cpp3e/review/04review.doc
+	cout << left; // left align
+	cout << setw(16) << "player name" << setw(10) << "awards" << setw(6) << "guess" << setw(6) << "lottery" << endl;
+	for (int i = 0; i < records; i++)
+	{
+		cout << setw(16) << gPlayerNames[i] << setw(10) << gPlayerAward[i] << setw(6) << gPlayerGuess[i] << setw(6) << gPlayerLottery[i] << endl;
+	}
+	cout << "*******************************************" << endl;
+}
+
+void DoAwardStatistics() {
+
+	// refer to:
+	// 1. find the formula for calculating std - standard deviation on https://en.wikipedia.org/wiki/Standard_deviation
+	// 2. programming exercises 7.11
+
+	// current number of records
+	int records = PLAYERS;
+	if (gtimesplayed < PLAYERS)
+	{
+		records = gtimesplayed;
+	}
+
+	int maximum = -1, minimum = -1, sum = -1;
+	double average = -1.0, std = -1.0;
+
+	// TODO 2. ===================================================
+	// find the maximum of gPlayerAward
+
+	// END OF TODO 2.=============================================
+
+	// find the minimum of gPlayerAward
+	minimum = gPlayerAward[0];
+	for (int i = 0; i < records; i++)
+	{
+		if (gPlayerAward[i] < minimum)
+			minimum = gPlayerAward[i];
+	}
+
+	// TODO 3. ===================================================
+	// find the sum and average of gPlayerAward
+
+	// END OF TODO 3.=============================================
+
+
+	// TODO 4. ===================================================
+	// find the std of gPlayerAward
+
+	// END OF TODO 4.=============================================
+
+
+	// print out the statistics of gPlayerAward
+	cout << "**********" << setw(2) << records << " lottery awards statistics **************" << endl;
+	// refer to: checkpoint 4.22-27
+	// 1. https://liveexample.pearsoncmg.com/liang/cpp3e/review/04review.doc
+	// 2. https://en.cppreference.com/w/cpp/io/manip/fixed
+	cout << left // left align
+		<< setprecision(2)
+		<< showpoint
+		<< fixed;
+	cout << setw(15) << "# of awards"
+		<< setw(16) << "total awards"
+		<< setw(12) << "top"
+		<< setw(8) << "bottom"
+		<< setw(10) << "average"
+		<< setw(10) << "std"
+		<< endl;
+
+	cout << setw(15) << records
+		<< setw(16) << sum
+		<< setw(12) << maximum
+		<< setw(8) << minimum
+		<< setw(10) << average
+		<< setw(10) << std
+		<< endl;
+
+	cout << "*****************************************************" << endl;
+
+}
+
+void SaveLottery() {
+	// refer to: https://liveexample.pearsoncmg.com/liang/cpp3e/html/SimpleFileOutput.html
+	int records = PLAYERS;
+	if (gtimesplayed < PLAYERS)
+	{
+		records = gtimesplayed;
+	}
+
+	ofstream savef;
+	savef.open("lottery-records.txt");
+	int i = 0;
+	for (i = 0; i < records - 1; i++)
+	{
+		// seperate each item with a single blank space!
+		savef << gPlayerNames[i] << " " << gPlayerAward[i] << " " << gPlayerLottery[i] << " " << gPlayerGuess[i] << endl;
+	}
+	savef << gPlayerNames[i] << " " << gPlayerAward[i] << " " << gPlayerLottery[i] << " " << gPlayerGuess[i];
+	savef.close();
+	cout << records << " lottery records saved!" << endl;
+}
+
+void LoadLottery() {
+	// refer to: 
+	// 1. https://liveexample.pearsoncmg.com/liang/cpp3e/html/SimpleFileInput.html
+	// 2. https://liveexample.pearsoncmg.com/liang/cpp3e/html/ReadAllData.html
+	ifstream loadf;
+
+	loadf.open("lottery-records.txt");
+
+	gtimesplayed = 0;
+	while (!loadf.eof())
+	{
+		loadf >> gPlayerNames[gtimesplayed] >> gPlayerAward[gtimesplayed] >> gPlayerLottery[gtimesplayed] >> gPlayerGuess[gtimesplayed];
+		gtimesplayed++;
+	}
+
+	loadf.close();
+	gcurrentplayer = gtimesplayed % PLAYERS;
+
+	cout << gtimesplayed << " lottery records loaded!" << endl;
+}
+
+void TestLottery(){
+	ifstream loadf;
+	loadf.open("test-records.txt");
+
+	gtimesplayed = 0;
+	while (!loadf.eof())
+	{
+		loadf >> gPlayerNames[gtimesplayed] >> gPlayerAward[gtimesplayed] >> gPlayerLottery[gtimesplayed] >> gPlayerGuess[gtimesplayed];
+		gtimesplayed++;
+	}
+
+	loadf.close();
+	gcurrentplayer = gtimesplayed % PLAYERS;
+
+	// Test your implementation
+	filebuf fbuf;
+	cout << "test started..." << endl;
+	fbuf.open("test-results.txt", ios::out);
+	auto oldbuf = cout.rdbuf(&fbuf);
+	DoAwardStatistics();
+	SortAwards();
+	cout.rdbuf(oldbuf);
+	cout << "test done!" << endl;
+}
