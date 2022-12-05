a4cad088a5a7d0e40ebca6ada59e85327d7275a8
ufidon <ufidon@gmail.com>
2022-12-04 20:19:52 -0500

    lab12 example added

---
+++ labs/lab12/demo/condestrain.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+using namespace std;
+
+class Person
+{
+public:
+  Person()
+  {
+    cout << "√ Constructing a person" << endl;
+  }
+
+  ~Person()
+  {
+    cout << "✕ Destructing a person" << endl;
+  }
+
+  void toString() const
+  {
+    cout << "= toString a person" << endl;
+  }
+};
+
+class Employee : public Person
+{
+public:
+  Employee()
+  {
+    cout << "√√ Constructing a employee" << endl;
+  }
+
+  ~Employee()
+  {
+    cout << "✕✕ Destructing a employee" << endl;
+  }
+
+  void toString() const
+  {
+    cout << "== toString a employee" << endl;
+  }
+};
+
+class Faculty : public Employee
+{
+public:
+  Faculty()
+  {
+    cout << "√√√ Constructing a faculty" << endl;
+  }
+
+  ~Faculty()
+  {
+    cout << "✕✕✕ Destructing a faculty" << endl;
+  }
+
+  void toString() const
+  {
+    cout << "=== toString a faculty" << endl;
+  }
+};
+
+void print(const Person &p)
+{
+  p.toString();
+}
+
+int main()
+{
+  cout << "create faculty:" << endl;
+  Faculty faculty;
+  cout << endl;
+
+  cout << "create employee:" << endl;
+  Employee employee;
+  cout << endl;
+
+  cout << "create person:" << endl;
+  Person person;
+  cout << endl;
+
+  cout << "print objects of subclasses:" << endl;
+  faculty.toString();
+  employee.toString();
+  cout << endl;
+
+  cout << "Start destructing all objects..." << endl;
+
+  return 0;
+}
